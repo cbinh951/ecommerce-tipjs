f87ad883a3fbac780b3fdbcc6e4bcb080a9391eb
binh <cbinh951@gmail.com>
2023-12-25 15:46:38 +0700

    handle error

--- controllers/access.controller.js
+++ controllers/access.controller.js
@@ -1,7 +1,20 @@
+const { CREATED } = require('../core/success.response');
 const AccessService = require('../services/access.service');
 class AccessController {
   signUp = async (req, res, next) => {
-    return res.status(201).json(await AccessService.signUp(req.body));
+    /**
+     * return res.status(200).json({
+     * message: '',
+     * metadata:
+     * })
+     */
+    new CREATED({
+      message: 'Registered OK!',
+      metadata: await AccessService.signUp(req.body),
+      options: {
+        limit: 10,
+      },
+    }).send(res);
   };
 }
 
