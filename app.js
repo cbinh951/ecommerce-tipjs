0b7008533bf196f412dce586de72c5b4ffd829a1
binh <cbinh951@gmail.com>
2023-12-25 15:06:22 +0700

    add middleware

--- app.js
+++ app.js
@@ -26,6 +26,20 @@ require('./dbs/init.mongodb');
 // init routes
 console.log('routes');
 app.use('/', require('./routes'));
-// handle error
 
+// handle error
+app.use((req, res, next) => {
+  const error = new Error('Not found');
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  const statusCode = error.status || 500;
+  return res.status(statusCode).json({
+    status: 'error',
+    code: statusCode,
+    message: error.message || 'Internal Server Error',
+  });
+});
 module.exports = app;
